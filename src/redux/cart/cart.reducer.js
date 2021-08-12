7af3f15f227a773e51f72d9a50ec35f68aa3c6d2
Boris DIBON <boris.dibon@gmail.com>
2021-08-12 23:15:08 +0200

    Add clearItem action to Checkout page

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -15,6 +15,13 @@ export default function cartReducer(state = INITIAL_STATE, action) {
         state,
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        ),
+      };
     default:
       return state;
   }
