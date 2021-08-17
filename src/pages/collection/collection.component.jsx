582573071aac5fbd5cd57ee6a24e33d22288c3a9
Boris DIBON <boris.dibon@gmail.com>
2021-08-17 20:34:09 +0200

    Use styled components for CollectionPage

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,18 +3,17 @@ import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.components";
 import { selectCollection } from "../../redux/shop/shop.selector";
-
-import "./collection.styles.scss";
+import { CollectionPageContainer } from "./collection.styles";
 
 const CollectionPage = ({ collection: { title, items } }) => (
-  <div className="collection-page">
+  <CollectionPageContainer>
     <h2 className="title">{title}</h2>
     <div className="items">
       {items.map(item => (
         <CollectionItem key={item.id} item={item} />
       ))}
     </div>
-  </div>
+  </CollectionPageContainer>
 );
 
 const mapStateToProps = (state, { match }) => ({
