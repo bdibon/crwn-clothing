2b100d9793ab73f112f8f3a601c7781c7bd0b2cb
Boris DIBON <boris.dibon@gmail.com>
2021-09-12 13:43:17 +0200

    Add compression middleware to our server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const compression = require("compression");
 const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
@@ -8,6 +9,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
